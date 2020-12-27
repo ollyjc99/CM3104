23f7e08345b378bd89a4d65c3834b4fb550adbe9
Oliver <44035033+ollyjc99@users.noreply.github.com>
2020-12-27 22:09:10 +0000

    gets first geographical feature for each caption

--- Part B/1835576_SQ1.py
+++ Part B/1835576_SQ1.py
@@ -1,9 +1,20 @@
 import spacy
+import json
 
-nlp = spacy.load("en_core_web_sm")
 
-text = ("Apple shift insurance liability toward manufacturers")
-doc = nlp(text)
+def main():
+    nlp = spacy.load("en_core_web_sm")
+    with open('json-capLatLong.json') as json_file:
+        data = json.load(json_file)
+        for p in data:
+            print('Caption: {}, Geographical feature {}'.format(p['caption'], get_geographical_feature(nlp(p['caption']))))
 
-for token in doc.ents:
-    print(token.text, token.label_)
+
+def get_geographical_feature(doc):
+    for ent in doc.ents:
+        if ent.label_ == 'FAC' or ent.label_ == 'GPE' or ent.label_ == 'LOC':
+            return ent.text.strip()
+
+
+if __name__ == '__main__':
+    main()
