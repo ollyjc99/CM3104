96f1e988ceda2c52aef8af20a33e759e775ede55
Oliver <44035033+ollyjc99@users.noreply.github.com>
2020-12-28 16:06:11 +0000

    Finished question 2

--- Part B/1835576_SQ2.py
+++ Part B/1835576_SQ2.py
@@ -1,9 +1,44 @@
 import spacy
 import json
+import random
 
 
 def main():
-    nlp = spacy.load("en_core_web_sm")
+    # GPE = Countries, Cities, States, etc.
+    # LOC = Non-GPE locations, mountain ranges, bodies of water
+    # FAC = Buildings, airports, highways, bridges
+
+    nlp = spacy.blank("en")
+    train_data = [
+        ("Grassland north of Eastdon", {
+            "entities": [(19, 26, "GPE")]
+        }),
+        ("Wall north of Hulne Park", {
+            "entities": [(14, 24, "GPE")]
+        }),
+        ("A45 north of South Solihull", {
+            "entities": [(13, 27, "GPE")]
+        })
+    ]
+    if "ner" not in nlp.pipe_names:
+        ner = nlp.create_pipe("ner")
+        nlp.add_pipe(ner, last=True)
+    else:
+        ner = nlp.get_pipe("ner")
+
+    for _, annotations in train_data:
+        for ent in annotations.get("entities"):
+            ner.add_label(ent[2])
+
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
+    with nlp.disable_pipes(*other_pipes):
+        optimizer = nlp.begin_training()
+        for i in range(50):
+            random.shuffle(train_data)
+            losses = {}
+            for text, annotations in train_data:
+                nlp.update([text], [annotations], drop=0.5, sgd=optimizer)
+
     with open('json-capLatLong.json') as json_file:
         data = json.load(json_file)
         tp, fp, fn = 0, 0, 0
@@ -20,10 +55,11 @@ def main():
                 fn += 1
             print('Caption: {}, Geographical feature: {}'.format(caption, feature))
 
+        print('TP: {}, FP: {}, FN: {}'.format(tp, fp, fn))
         precision = tp / (tp + fp)
-        print('Precision = {}'.format(precision))
+        print('Precision = {:.2f}'.format(precision))
         recall = tp / (tp + fp + fn)
-        print('Recall = {}'.format(recall))
+        print('Recall = {:.2f}'.format(recall))
 
         print('F1 = {:.2f}'.format(2 * precision * recall / (precision + recall)))
 
