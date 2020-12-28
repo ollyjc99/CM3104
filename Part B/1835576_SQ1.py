30686645f1852434ff8597ab50e0459341c9ab3d
Oliver <44035033+ollyjc99@users.noreply.github.com>
2020-12-28 17:19:06 +0000

    Added a separate function to return caption and metrics

--- Part B/1835576_SQ1.py
+++ Part B/1835576_SQ1.py
@@ -29,6 +29,28 @@ def main(file):
         print('F1 = {:.2f}'.format(2 * precision * recall / (precision + recall)))
 
 
+def get_metrics(nlp, file):
+    captions = []
+    with open(file) as json_file:
+        data = json.load(json_file)
+        tp, fp, fn = 0, 0, 0
+        for p in data:
+            caption = p['caption']
+            feature = get_geographical_feature(nlp(p['caption']))
+            toponym = p['ground truth toponym']
+            if feature:
+                if feature == toponym:
+                    tp += 1
+                else:
+                    fp += 1
+            else:
+                fn += 1
+
+            captions.append({"caption": caption, "feature": feature})
+
+    return captions, tp, fp, fn
+
+
 def get_geographical_feature(doc):
     for ent in doc.ents:
         if ent.label_ == 'FAC' or ent.label_ == 'GPE' or ent.label_ == 'LOC':
