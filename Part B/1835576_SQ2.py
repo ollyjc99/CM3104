b7ac5e129075085fe69ca9b4cbbac7e6fdd041b0
Oliver <44035033+ollyjc99@users.noreply.github.com>
2020-12-28 13:58:59 +0000

    Started Q2

---
+++ Part B/1835576_SQ2.py
@@ -0,0 +1,38 @@
+import spacy
+import json
+
+
+def main():
+    nlp = spacy.load("en_core_web_sm")
+    with open('json-capLatLong.json') as json_file:
+        data = json.load(json_file)
+        tp, fp, fn = 0, 0, 0
+        for p in data:
+            caption = p['caption']
+            feature = get_geographical_feature(nlp(p['caption']))
+            toponym = p['ground truth toponym']
+            if feature:
+                if feature == toponym:
+                    tp += 1
+                else:
+                    fp += 1
+            else:
+                fn += 1
+            print('Caption: {}, Geographical feature: {}'.format(caption, feature))
+
+        precision = tp / (tp + fp)
+        print('Precision = {}'.format(precision))
+        recall = tp / (tp + fp + fn)
+        print('Recall = {}'.format(recall))
+
+        print('F1 = {:.2f}'.format(2 * precision * recall / (precision + recall)))
+
+
+def get_geographical_feature(doc):
+    for ent in doc.ents:
+        if ent.label_ == 'FAC' or ent.label_ == 'GPE' or ent.label_ == 'LOC':
+            return ent.text.strip()
+
+
+if __name__ == '__main__':
+    main()
