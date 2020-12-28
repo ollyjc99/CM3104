76c40e0c890f858d439309be056148782123f060
Oliver <44035033+ollyjc99@users.noreply.github.com>
2020-12-28 16:41:21 +0000

    Added argparse

--- Part B/1835576_SQ2.py
+++ Part B/1835576_SQ2.py
@@ -1,45 +1,17 @@
 import spacy
 import json
 import random
+import argparse
 
 
-def main():
+def main(file):
     # GPE = Countries, Cities, States, etc.
     # LOC = Non-GPE locations, mountain ranges, bodies of water
     # FAC = Buildings, airports, highways, bridges
 
-    nlp = spacy.blank("en")
-    train_data = [
-        ("Grassland north of Eastdon", {
-            "entities": [(19, 26, "GPE")]
-        }),
-        ("Wall north of Hulne Park", {
-            "entities": [(14, 24, "GPE")]
-        }),
-        ("A45 north of South Solihull", {
-            "entities": [(13, 27, "GPE")]
-        })
-    ]
-    if "ner" not in nlp.pipe_names:
-        ner = nlp.create_pipe("ner")
-        nlp.add_pipe(ner, last=True)
-    else:
-        ner = nlp.get_pipe("ner")
+    nlp = get_new_model()
 
-    for _, annotations in train_data:
-        for ent in annotations.get("entities"):
-            ner.add_label(ent[2])
-
-    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
-    with nlp.disable_pipes(*other_pipes):
-        optimizer = nlp.begin_training()
-        for i in range(50):
-            random.shuffle(train_data)
-            losses = {}
-            for text, annotations in train_data:
-                nlp.update([text], [annotations], drop=0.5, sgd=optimizer)
-
-    with open('json-capLatLong.json') as json_file:
+    with open(file) as json_file:
         data = json.load(json_file)
         tp, fp, fn = 0, 0, 0
         for p in data:
@@ -70,5 +42,41 @@ def get_geographical_feature(doc):
             return ent.text.strip()
 
 
+def get_new_model():
+    nlp = spacy.blank("en")
+    train_data = [
+        ("Grassland north of Eastdon", {
+            "entities": [(19, 26, "GPE")]
+        }),
+        ("Wall north of Hulne Park", {
+            "entities": [(14, 24, "GPE")]
+        }),
+        ("A45 north of South Solihull", {
+            "entities": [(13, 27, "GPE")]
+        })
+    ]
+    if "ner" not in nlp.pipe_names:
+        ner = nlp.create_pipe("ner")
+        nlp.add_pipe(ner, last=True)
+    else:
+        ner = nlp.get_pipe("ner")
+
+    for _, annotations in train_data:
+        for ent in annotations.get("entities"):
+            ner.add_label(ent[2])
+
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "ner"]
+    with nlp.disable_pipes(*other_pipes):
+        optimizer = nlp.begin_training()
+        for i in range(50):
+            random.shuffle(train_data)
+            for text, annotations in train_data:
+                nlp.update([text], [annotations], drop=0.5, sgd=optimizer)
+    return nlp
+
+
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='Arguments')
+    parser.add_argument('json_file', type=str, help='Path for JSON file')
+    args = parser.parse_args()
+    main(args.json_file)
