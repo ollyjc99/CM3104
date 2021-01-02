dedc40709119bb82e39bad199e8dc2d731b4ed56
ollyjc99 <44035033+ollyjc99@users.noreply.github.com>
2021-01-02 18:20:36 +0000

    Update normalize_data.py

--- Part A/normalize_data.py
+++ Part A/normalize_data.py
@@ -1,5 +1,33 @@
+import json
+import os
+import time
+
+
 def main():
-    pass
+    root = 'fitbit_data/'
+    json_dict = get_jsons(root)
+    sorted_dict = sort_json(json_dict)
+
+
+def get_jsons(root):
+    json_dict = {}
+    for person in os.scandir(root):
+        json_dict[person.name] = {}
+        for data in os.scandir(person):
+            json_dict[person.name][data.name] = []
+            for file in os.scandir(data):
+                with open(file, "r") as read_file:
+                    json_dict[person.name][data.name].append(json.load(read_file))
+    return json_dict
+
+
+def sort_json(json):
+    new_dict = {}
+    for person in json.keys():
+        new_dict[person] = {}
+        for data in json[person].keys():
+            for file in json[person][data]:
+                print(file)
 
 
 if __name__ == "__main__":
