670b2354eb30fe8ce91d85c21a225792ed449bc8
ollyjc99 <44035033+ollyjc99@users.noreply.github.com>
2021-01-08 17:46:40 +0000

    removed last of pprint

--- Part A/q1t2cii.py
+++ Part A/q1t2cii.py
@@ -10,50 +10,31 @@ pipeline = [
         '$project': {
             '_id': 1,
             'datetime': 1,
-            'month': {
-                '$toInt': {
-                    '$substr': [
-                        '$datetime', 5, 2
-                    ]
-                }
-            },
+            'month': {'$toInt': {'$substr': ['$datetime', 5, 2]}},
             'p_1_s': '$person_1_fitbit.steps',
             'p_2_s': '$person_2_fitbit.steps',
             'time_of_day': 1
         }
-    }, {
+    },
+    {
         '$match': {
             'time_of_day': 'morning',
-            'month': 6
+            'month': 8
         }
-    }, {
+    },
+    {
         '$group': {
             '_id': '$month',
-            'person_1_steps': {
-                '$sum': '$p_1_s'
-            },
-            'person_2_steps': {
-                '$sum': '$p_2_s'
-            }
+            'person_1_steps': {'$sum': '$p_1_s'},
+            'person_2_steps': {'$sum': '$p_2_s'}
         }
-    }, {
+    },
+    {
         '$project': {
-            'most_steps': {
-                '$cond': {
-                    'if': {
-                        '$gt': [
-                            '$person_1_steps', '$person_2_steps'
-                        ]
-                    },
+            'most_steps': {'$cond': {'if': {'$gt': ['$person_1_steps', '$person_2_steps']},
                     'then': 'Person 1',
-                    'else': 'Person 2'
-                }
-            },
-            'steps': {
-                '$max': [
-                    '$person_1_steps', '$person_2_steps'
-                ]
-            }
+                    'else': 'Person 2'}},
+            'steps': {'$max': ['$person_1_steps', '$person_2_steps']}
         }
     }
 ]
